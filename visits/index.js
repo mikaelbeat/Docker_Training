8b3a42b2e33c93c8f6f492dfa778098360aec541
Ryynanen Petri <petri.ryynanen@tieto.com>
2019-08-12 18:47:04 +0300

    Update

--- visits/index.js
+++ visits/index.js
@@ -1,17 +1,23 @@
-const express = require("express")
-const redis = require("redis")
+const express = require("express");
+const redis = require("redis");
 const app = express();
-const client = redis.createClient();
+const process = require("process");
+
+const client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
 
 client.set("visits", 0);
 
 app.get("/", (req, res) => {
-    clientInformation.get("visits", (err, visits) => {
+    process.exit(0);
+    client.get("visits", (err, visits) => {
         res.send("Number of visits is " + visits);
-        client.set("Visits", parseInt(visits) + 1);
+        client.set("visits", parseInt(visits) + 1);
     });
 });
 
-app.listen(8081, () =>{
-    console.log("Listening on port 8081!");
+app.listen(8080, () =>{
+    console.log("Listening on port 8080!");
 });
