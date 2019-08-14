75f5e5fd3a206dc09eb5540231b849811ed348d4
Ryynanen Petri <petri.ryynanen@tieto.com>
2019-08-14 15:40:09 +0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          SUffeli
         </p>
         <a
           className="App-link"
